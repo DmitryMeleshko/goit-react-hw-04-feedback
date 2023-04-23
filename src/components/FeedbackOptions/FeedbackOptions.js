35d92cb6edb1b8981f1a29e51922366086a1d3a3
DmitryMeleshko <meleshko.triolcorp@gmail.com>
2023-04-23 10:11:49 +0300

    form/proptype

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -18,6 +18,6 @@ export const FeedbackOptions = ({options, onLeaveFeedback}) => {
     );
 };
 FeedbackOptions.propTypes = {
-    options: PropTypes.string.isRequired,
+    options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     onLeaveFeedback: PropTypes.string.isRequired,
 };
